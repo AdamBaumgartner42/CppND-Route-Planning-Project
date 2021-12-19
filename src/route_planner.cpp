b6861384fa3990c943dbf44d84945913ca71988e
AdamBaumgartner42 <adambaumgartner@gmail.com>
2021-12-19 13:24:39 -0500

    TODO #5 draft - test not passing

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -10,11 +10,11 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
     end_y *= 0.01;
     
     // DONE: 6-22-21
-    // OK 2.1: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
-    // OK 2.2: Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
-    
-    start_node = &m_Model.FindClosestNode(start_x, start_y);
-    end_node = &m_Model.FindClosestNode(end_x, end_y);
+    // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
+    // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
+
+    this->start_node = &m_Model.FindClosestNode(start_x, start_y); 
+    this->end_node = &m_Model.FindClosestNode(end_x, end_y);
 
 }
 
@@ -26,30 +26,25 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 // - You can use the distance to the end_node for the h value.
 // - Node objects have a distance method to determine the distance to another node.
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node){
-    return node->distance(*end_node);
+    return node->distance(*this->end_node);
 }
 
 // Done 6-28-21
 // TODO 4: Complete the AddNeighbors method to expand the current node by adding all unvisited neighbors to the open list.
 void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
-    // Use FindNeighbors method of the current_node to populate current node.neighboors vector with all the neighboors.
-    current_node->FindNeighbors();
-    // For each node in current_node.neighbors ...
-    for (Node* neighbor : current_node->neighbors){
-        // Set the parent Node.
+
+   current_node->FindNeighbors(); // adds nodes to curren_node's neighbor list
+
+    for (auto neighbor : current_node->neighbors){
         neighbor->parent = current_node;
-        // Set the h value
-        neighbor->h_value = RoutePlanner::CalculateHValue(neighbor);
-        // Set the g value. current_g + dist to current
-        neighbor->g_value = current_node->g_value + neighbor->distance(&current_node); // expects Node, not Node*
-        // Add the node to open_list 
-        open_list.push_back(neighbor); 
-        // Set the visted value to true
+        neighbor->h_value = CalculateHValue(neighbor);
+        neighbor->g_value = current_node->g_value + current_node->distance(*neighbor);
+        this->open_list.emplace_back(neighbor);
         neighbor->visited = true;
     }
 }
 
-
+// ** Test Not Passing ** : 12-19-21
 // TODO 5: Complete the NextNode method to sort the open list and return the next node.
 // Tips:
 // - Sort the open_list according to the sum of the h value and g value.
@@ -58,6 +53,20 @@ void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
 // - Return the pointer.
 
 RouteModel::Node *RoutePlanner::NextNode() {
+    // sort open_list to have the smallest sum at the end
+    std::sort(this->open_list.begin(), this->open_list.end(),[]( auto& a, auto& b){
+        return (a->h_value + a->g_value) > (b->h_value + b->g_value);
+    });
+
+    // add last element to a pointer
+    int n = this->open_list.size();
+    RouteModel::Node *tmp = open_list[n-1];
+
+    // remove last element
+    this->open_list.pop_back(); 
+
+    // return the pointer
+    return tmp;
 
 }
 
